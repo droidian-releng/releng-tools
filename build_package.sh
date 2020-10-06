13882d8c19ad436d944405c0650f7d07e4b86c05
Eugenio Paolantonio (g7) <me@medesimo.eu>
2020-10-07 01:23:21 +0200

    [releng-tools] build-package: always strip epoch when looking for the upstream tag
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- build_package.sh
+++ build_package.sh
@@ -123,7 +123,7 @@ if [ -e "debian/source/format" ] && grep -q "quilt" debian/source/format; then
 	package_orig_version=$(echo "${package_info}" | awk '{ print $2 }' | cut -d- -f1 | sed 's/(//')
 
 	package_orig_version_tag="${package_orig_version/~/_}"
-	package_orig_version_tag="${package_orig_version_tag/:/%}"
+	package_orig_version_tag="$(echo ${package_orig_version_tag} | cut -d'%' -f2-)"
 
 	# git archive doesn't support submodules, which is not ideal.
 	# Workaround this by creating a new worktree from the upstream tag,
